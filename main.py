8f48c01b80f6604f1ed659e602dc209a0f22b769
Nathan <124796583+naccaratonathan@users.noreply.github.com>
2023-09-15 08:22:38 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,38 @@
+def on_button_pressed_a():
+    bucket.move(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    bucket.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+drop: game.LedSprite = None
+bucket: game.LedSprite = None
+game.set_score(0)
+bucket = game.create_sprite(2, 4)
+
+def on_forever():
+    global drop
+    drop = game.create_sprite(randint(0, 4), -1)
+    drop.turn(Direction.RIGHT, 90)
+    for index in range(4):
+        drop.move(1)
+        basic.pause(750)
+        if bucket.is_touching(drop):
+            game.add_score(1)
+            drop.delete()
+    if drop.is_touching_edge():
+        music.play(music.create_sound_expression(WaveShape.SQUARE,
+                1989,
+                1,
+                255,
+                255,
+                1291,
+                SoundExpressionEffect.NONE,
+                InterpolationCurve.LINEAR),
+            music.PlaybackMode.UNTIL_DONE)
+        drop.delete()
+        basic.clear_screen()
+        basic.show_string("GAME OVER")
+        basic.show_string("" + str((game.score())))
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    bucket.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    bucket.move(1)
+})
+let drop: game.LedSprite = null
+let bucket: game.LedSprite = null
+game.setScore(0)
+bucket = game.createSprite(2, 4)
+basic.forever(function () {
+    drop = game.createSprite(randint(0, 4), -1)
+    drop.turn(Direction.Right, 90)
+    for (let index = 0; index < 4; index++) {
+        drop.move(1)
+        basic.pause(750)
+        if (bucket.isTouching(drop)) {
+            game.addScore(1)
+            drop.delete()
+        }
+    }
+    if (drop.isTouchingEdge()) {
+        music.play(music.createSoundExpression(
+        WaveShape.Square,
+        1989,
+        1,
+        255,
+        255,
+        1291,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), music.PlaybackMode.UntilDone)
+        drop.delete()
+        basic.clearScreen()
+        basic.showString("GAME OVER")
+        basic.showString("" + (game.score()))
+    }
+})
